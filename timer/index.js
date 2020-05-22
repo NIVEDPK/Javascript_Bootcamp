9112b851577e625669bdaf31026fecebf382fff1
NIVEDPK <nivedpkkannan31@gmail.com>
2020-05-22 21:30:20 +0530

    timer reduce without animation

--- timer/index.js
+++ timer/index.js
@@ -1,1 +1,32 @@
-console.log('Hi there!');
+class Timer {
+  constructor(durationInput, startTimer, pauseTimer) {
+    this.durationInput = durationInput;
+    this.startTimer = startTimer;
+    this.pauseTimer = pauseTimer;
+
+    this.startTimer.addEventListener("click", this.start);
+    this.pauseTimer.addEventListener("click", this.pause);
+  }
+
+  start = () => {
+    this.tick();
+    this.intervalId = setInterval(this.tick, 1000);
+  };
+
+  tick = () => {
+    console.log(this.durationInput.value);
+    this.durationInput.value = parseInt(this.durationInput.value) - 1;
+  };
+
+  pause = () => {
+    clearInterval(this.intervalId);
+  };
+}
+
+const durationInput = document.querySelector("#duration");
+
+const startTimer = document.querySelector("#start");
+
+const pauseTimer = document.querySelector("#pause");
+
+new Timer(durationInput, startTimer, pauseTimer);
